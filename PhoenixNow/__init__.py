1ab536cd7dab7f107e011e42335192276151e1ce
Nicholas Day <nick@nickendo.com>
2016-08-16 15:35:55 -0400

    Made email settings environment variables

--- PhoenixNow/__init__.py
+++ PhoenixNow/__init__.py
@@ -16,10 +16,10 @@ def create_app(config_object):
     app.register_blueprint(admin,url_prefix='/admin')
 
     ### Configuration for flask-mail | "SMPT" Settings | This is the email account that sends emails ###
-    app.config["MAIL_SERVER"] = "mail.privateemail.com" # specifies email domain. "smtp.gmail.com" for a gmail account
+    app.config["MAIL_SERVER"] = os.environ.get('emailserver') # specifies email domain. "smtp.gmail.com" for a gmail account
     app.config["MAIL_PORT"] = 465
     app.config["MAIL_USE_SSL"] = True
-    app.config["MAIL_USERNAME"] = 'support@chadali.me' # your email address
+    app.config["MAIL_USERNAME"] = os.environ.get('email') # your email address
     app.config["MAIL_PASSWORD"] = os.environ.get('emailpass') # email password
     ### Configuration for flask-mail | If you're using your own email, in views.py change sender='support@chadali.me' to your email ###
 
