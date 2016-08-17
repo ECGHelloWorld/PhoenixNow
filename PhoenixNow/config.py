dc28efa6bfa4c5e69d896862768630a92bac4a11
Nicholas Day <nick@nickendo.com>
2016-08-16 20:31:58 -0400

    Added contact template and finally switched to actual db not memory

--- PhoenixNow/config.py
+++ PhoenixNow/config.py
@@ -1,13 +1,13 @@
 class Config(object):
     DEBUG = False
     TESTING = False
-    DATABASE_URI = 'sqlite://:memory:'
+    SQLALCHEMY_DATABASE_URI = 'sqlite://:memory:'
     SECRET_KEY = 'idontknowwhatthisis'
     SECURITY_PASSWORD_SALT = "fortoken"
     MAIL_DEFAULT_SENDER = "support@chadali.me"
 
 class ProductionConfig(Config):
-    DATABASE_URI = 'sqlite:///data.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
 
 class DevelopmentConfig(Config):
     DEBUG = True
