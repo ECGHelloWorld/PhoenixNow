6463512eb25f7dd31ca2a28c3a9f0cdc507420c7
chadali <23alic@gmail.com>
2016-08-22 14:53:54 -0400

    whoops

--- PhoenixNow/model.py
+++ PhoenixNow/model.py
@@ -40,7 +40,7 @@ class User(db.Model):
         self.checkedin = False
         self.verified = False
         self.schedule_verified = False
-        self.schedule = "M:T:W:R:F (default)"
+        self.schedule = "M:T:W:R:F"
         self.schedule_monday = True
         self.schedule_tuesday = True
         self.schedule_wednesday = True
