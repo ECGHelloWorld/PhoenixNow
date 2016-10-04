8d2a07a051af6eb9d9efd902951af2582f24bee8
Nicholas Day <nick@nickendo.com>
2016-10-04 13:31:46 -0400

    Added a function to view past dates

--- PhoenixNow/admin.py
+++ PhoenixNow/admin.py
@@ -1,27 +1,46 @@
 from flask import Flask, render_template, Blueprint, redirect, url_for, flash, request
+from datetime import timedelta
 from PhoenixNow.decorators import login_notrequired, admin_required, check_verified, check_notverified
 from PhoenixNow.model import db, User, Checkin
 from PhoenixNow.user import get_weekly_checkins
 from flask_login import login_required, login_user, logout_user
-from PhoenixNow.forms import UserForm
+from PhoenixNow.forms import UserForm, CalendarForm
 import datetime
 import os
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
-@admin.route('/')
+@admin.route('/', methods=['GET', 'POST'])
 @login_required
 @admin_required
 def home():
-    today = datetime.date.today()
+    form = CalendarForm()
+    if request.method == 'POST':
+        try:
+            stringdate = form.date.data
+            searchdate = datetime.datetime.strptime(stringdate, '%Y-%m-%d').date()
+        except:
+            return "Improper Date Format" + form.date.data
+
+        if request.form['submit'] == "Next Week":
+            searchdate = searchdate + timedelta(days=7)
+            form.date.data = searchdate
+
+        if request.form['submit'] == "Previous Week":
+            searchdate = searchdate + timedelta(days=-7)
+            form.date.data = searchdate
+
+    if request.method == 'GET':
+        searchdate = datetime.date.today()
     users = User.query.all()
     users.sort(key=lambda user: (user.grade, user.lastname)) # sort by grade and name
-    checkins = Checkin.query.filter(Checkin.checkin_timestamp>=today).all()
+    checkins = Checkin.query.filter(Checkin.checkin_timestamp>=searchdate).all()
     checkins.sort(key=lambda checkin: (checkin.user.lastname)) # sort by grade and name
-    weekly_checkins = get_weekly_checkins(today) # look at user.py
+    weekly_checkins = get_weekly_checkins(searchdate) # look at user.py
     weekly_checkins.update_database() # look at user.py
-    return render_template('admin.html', users=users, checkins=checkins)
+    return render_template('admin.html', searchdate=searchdate, form=form, users=users, checkins=checkins)
 
+                 
 @admin.route('/push')
 @login_required
 @admin_required
