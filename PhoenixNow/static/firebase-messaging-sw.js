801cae84e84102a0f7b411e0a47f7b8300270b6c
ali <23alic@gmail.com>
2016-11-06 03:45:22 -0500

    service worker

--- PhoenixNow/static/firebase-messaging-sw.js
+++ PhoenixNow/static/firebase-messaging-sw.js
@@ -11,20 +11,19 @@ var config = {
 firebase.initializeApp(config);
 
 
-//const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
-//messaging.setBackgroundMessageHandler(function(payload) {
- // console.log('[firebase-messaging-sw.js] Received background message ', payload);
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
- // const notificationTitle = 'Background Message Title';
- // const notificationOptions = {
-  //  body: 'Background Message body.',
-  //  icon: '/firebase-logo.png'
-  //};
+  const notificationTitle = 'PhoenixNow SignIn';
+  const notificationOptions = {
+    body: 'Background Message body.'
+  };
 
-  //return self.registration.showNotification(notificationTitle,
- //     notificationOptions);
-//});
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
 
 //curl 
 
