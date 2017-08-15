3b2dd4ac9c4caac343fb47c337b6409b5c6a4941
Nicholas Day <nick@nickendo.com>
2017-08-14 23:13:44 -0400

    Fix server configs

--- PhoenixNow/config.py
+++ PhoenixNow/config.py
@@ -2,13 +2,12 @@ import os
 
 class Config(object):
     DEBUG = False
-    TESTING = False
     SECRET_KEY = 'idontknowwhatthisis'
     SECURITY_PASSWORD_SALT = "fortoken"
     MAIL_DEFAULT_SENDER = "support@phoenixnow.org"
-    MAIL_SERVER = os.environ.get('EMAILSERVER')
     MAIL_PORT = 465
     MAIL_USE_SSL = True
+    MAIL_SERVER = os.environ.get('EMAILSERVER')
     MAIL_USERNAME = os.environ.get('EMAIL')
     MAIL_PASSWORD = os.environ.get('EMAILPASS')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../data.db'
